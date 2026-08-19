509439385bd92e726d1d50077beea70154cb276d
Divyansh Saxena <129607728+divyanshsaxena21@users.noreply.github.com>
2026-08-19 14:47:24 +0530

    Add: plus-one - submission-0

---
+++ Data Structures & Algorithms/plus-one/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        num = 0
+        for i in digits:
+            num = (num*10) + i
+        
+        num += 1
+
+        res = []
+        while (num):
+            res.append(num%10)
+            num //= 10
+        
+        return res[::-1]
